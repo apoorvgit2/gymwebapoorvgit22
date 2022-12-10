6e43b8b6c27c1b95680fd7f878afb3a7c9f08d2f
Apoorv Yashovardhan Mishra <115280960+apoorvgit2@users.noreply.github.com>
2022-12-10 23:59:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('views', path.join(__dirname, 'views')) // Set the views directory
 app.get('/', (req, res)=>{
    
     const params = { }
-    res.status(200).render('home.pug', params);
+    res.status(200).render('index.pug', params);
 })
 app.get('/contact', (req, res)=>{
    
